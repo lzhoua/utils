3e258a787b8525da3261bec263b5575412e0ac3e
龙舟 <longzhou@longzhoudeMacBook-Pro.local>
2022-04-27 22:50:12 +0800

    updata: 增加一个几分钟内写出来的简版event

---
+++ event.js
@@ -0,0 +1,49 @@
+const event = {
+    listenMap: {},
+    /**
+     * 声明一个监听
+     * @param {*} key 时间key
+     * @param {*} listener 监听者
+     * @param {*} options 配置参数
+     */
+    on (key, listener, options = {}) {
+        const { cover = false } = options
+        // 如果不是覆盖 & 已经存在key
+        if (!cover && this.listenMap[key]) {
+            console.warn(`【${key}】 already exists`)
+            return
+        }
+        this.listenMap[key] = listener
+    },
+    /**
+     * 声明一个一次性的监听
+     */
+    once (key, fn) {
+        this.on(key, (...args) => {
+            fn(args)
+            this.remove(key)
+        })
+    },
+    /**
+     * 发送一个消息
+     */
+    emit (key, value) {
+        if (!this.listenMap[key]) {
+            console.warn(`【${key}】not registered`);
+            return
+        }
+        this.listenMap[key](value)
+    },
+    /**
+     * 移除某个监听
+     */
+    remove (key) {
+        delete this.listenMap[key]
+    },
+    /**
+     * 清除所有的监听
+     */
+    clear () {
+        this.listenMap = {}
+    }
+}
